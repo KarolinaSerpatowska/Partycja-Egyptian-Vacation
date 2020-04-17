a0969619ac327ec7286bad5c9169069210865764
KarolinaSerpatowska <4ka@wp.pl>
2020-04-17 13:40:43 +0200

    szymon fight scripts

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerInput : MonoBehaviour
+public class PlayerInput : AttackingSystemPlayer
 {
     public float dodgeRate = 0.9f;
     private float nextDodge;
@@ -25,6 +25,9 @@ public class PlayerInput : MonoBehaviour
     private Vector3 moveVector;
     public float moveSpeed = 2f;
 
+    [SerializeField] Interactable focusedOn = null;
+    [SerializeField] Transform playerTransform;
+    bool isFocus = false;
     void Start()
     {
         anim = this.GetComponent<Animator>();
@@ -33,6 +36,7 @@ public class PlayerInput : MonoBehaviour
         //poniezsze na wszelki wypadek, zaczecia nowej gry nie po raz pierwszy
         anim.SetBool("IsDeath", false);
         anim.SetBool("IsAttack", false);
+        playerTransform = PlayerManger.instance.player.transform;
     }
 
     public void CheckInput()
@@ -93,9 +97,11 @@ public class PlayerInput : MonoBehaviour
 
     public void CheckInputNotMove()
     {
+
         if (Input.GetButton("Attack1") && Time.time > nextAttack)
         {
             //attack
+            Attack();
             nextAttack = Time.time + attackRate;
             anim.SetBool("IsAttack", true);
             Debug.Log("attackButton");
@@ -104,6 +110,7 @@ public class PlayerInput : MonoBehaviour
         if (Input.GetButton("Dodge") && Time.time > nextDodge)
         {
             //dodge
+
             nextDodge = Time.time + dodgeRate;
             Debug.Log("dodgeButton");
         }
@@ -113,6 +120,43 @@ public class PlayerInput : MonoBehaviour
             //opcje
             Debug.Log("optionsButton");
         }
+
+        checkRightButtonClick();
     }
+    void checkRightButtonClick()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            LayerMask layer = 0;
+            layer = LayerMask.NameToLayer("Interacable");
+            if (layer == 0)
+                Debug.LogError("Problem with layer");
+            if (Physics.Raycast(ray, out hit, layer))
+            {
+                Interactable interactable = hit.collider.GetComponent<Interactable>();
+                if (!interactable)
+                    Debug.LogWarning("no Interactable hit.");
+                else
+                {
+                    SetFocusOn(interactable);
+                    interactable.OnFocused(playerTransform);
+                }
+            }
+
+        }
+    }
+    void SetFocusOn(Interactable interactable)
+    {
+        focusedOn = interactable;
+        isFocus = true;
+    }
+    void Removefocus()
+    {
+        focusedOn = null;
+        isFocus = false;
+    }
+
 
 }
